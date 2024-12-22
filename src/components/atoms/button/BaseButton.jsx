2a9ee1931f49a23c29feb87ed01e70629e83efc6
kuraunDeveloper <kuraunDeveloper@gmail.com>
2024-12-22 15:35:41 +0900

    ボタンコンポーネントを新規追加（Atom）

---
+++ src/components/atoms/button/BaseButton.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+    color: #fff;
+    border: none;
+    padding: 8px 24px;
+    border-radius: 9999px;
+    cursor: pointer;
+    outline: none;
+    &:hover {
+        opacity: 0.8;
+    }
+`;
